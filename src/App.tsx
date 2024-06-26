ca73090d01d9dc0dc2bcee6a6694aba6426072d0
jolochis <jolochis@gmail.com>
2024-06-26 00:04:36 -0700

    funcionalidad busqueda y get

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import "./App.css";
+import Hero from "./components/Layout/Hero";
 import { PostList } from "./components/PostList";
+import SingleElement from "./components/PostDetail";
 
 function App() {
   return (
-    <>
+    <div className="bg-slate-600">
+      <Hero />
       <PostList></PostList>
-    </>
+    </div>
   );
 }
 
--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,5 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getPosts } from "../services/postService";
 
 export const PostList = () => {
-  return <div>PostList</div>;
+  const [post, setPost] = useState([] as any[]);
+
+  useEffect(() => {
+    (async () => {
+      const allPosts = await getPosts();
+      setPost(allPosts);
+    })();
+  }, []);
+
+  return (
+    /*  <section className="text-gray-600 body-font">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-col text-center w-full mb-20">
+          <h1 className="text-2xl font-medium title-font mb-4 text-gray-900">
+            titulo
+          </h1>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+            contenido
+          </p>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Fecha:</p>
+        </div>
+      </div>
+    </section> */
+    <section className="text-gray-600 body-font">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-col text-center w-full mb-20">
+          {post &&
+            post.map((item) => {
+              return (
+                <div key={item.id}>
+                  <h1 className="text-2xl font-medium title-font mb-4 text-gray-900">
+                    {item.titulo}
+                  </h1>
+                  <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+                    contenido
+                  </p>
+                  <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+                    Fecha:
+                  </p>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    </section>
+  );
 };
